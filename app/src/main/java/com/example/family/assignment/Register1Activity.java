b1421cea276080f5cca5375107e4e2a11c4cb3e8
Yv Heng <kongyh-wa15@student.tarc.edu.my>
2017-11-07 20:40:02 +0800

    user interface

--- app/src/main/java/com/example/family/assignment/Register1Activity.java
+++ app/src/main/java/com/example/family/assignment/Register1Activity.java
@@ -3,11 +3,11 @@ package com.example.family.assignment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class RegisterActivity extends AppCompatActivity {
+public class Register1Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_register1);
     }
 }
