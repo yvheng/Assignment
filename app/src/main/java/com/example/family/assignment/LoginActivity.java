62beaba6e985c18419b05a6c94264c4563c61029
Yv Heng <kongyh-wa15@student.tarc.edu.my>
2017-11-08 01:12:21 +0800

    user interface

--- app/src/main/java/com/example/family/assignment/LoginActivity.java
+++ app/src/main/java/com/example/family/assignment/LoginActivity.java
@@ -1,13 +1,52 @@
 package com.example.family.assignment;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText editTextUs, editTextPassword;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editTextUs = (EditText)findViewById(R.id.editTextUs);
+        editTextPassword = (EditText)findViewById(R.id.editTextPassword);
+    }
+
+    public void login(View v){
+        intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void register(View v){
+        intent = new Intent(this, Register1Activity.class);
+        startActivity(intent);
+    }
+
+    public void forgetPassword(View v){
+        intent = new Intent(this, RecoveryActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 }
--- app/src/main/java/com/example/family/assignment/Register1Activity.java
+++ app/src/main/java/com/example/family/assignment/Register1Activity.java
@@ -1,13 +1,56 @@
 package com.example.family.assignment;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class Register1Activity extends AppCompatActivity {
+    private Intent intent;
+    private EditText editTextUs, editTextPw, editTextCfPw, editTextEmail;
+    private RadioButton radioButtonMale, radioButtonFemale;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register1);
+
+        editTextUs = (EditText)findViewById(R.id.editTextUs);
+        editTextPw = (EditText)findViewById(R.id.editTextPassword);
+        editTextCfPw = (EditText)findViewById(R.id.editTextCfPw);
+        editTextEmail = (EditText)findViewById(R.id.editTextEmail);
+        radioButtonMale = (RadioButton)findViewById(R.id.radioButtonMale);
+        radioButtonFemale = (RadioButton)findViewById(R.id.radioButtonFemale);
+    }
+
+    public void next(View v){
+        intent = new Intent(this, Register2Activity.class);
+        startActivity(intent);
+    }
+
+    public void reset(View v){
+        editTextUs.setText("");
+        editTextPw.setText("");
+        editTextCfPw.setText("");
+        editTextEmail.setText("");
+        radioButtonMale.setChecked(false);
+        radioButtonFemale.setChecked(false);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 }
