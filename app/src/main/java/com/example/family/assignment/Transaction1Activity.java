55507d33a4596a2b30d3527360bad924c214ed3e
Yv Heng <kongyh-wa15@student.tarc.edu.my>
2017-11-10 00:51:58 +0800

    user interface

--- app/src/main/java/com/example/family/assignment/Transaction1Activity.java
+++ app/src/main/java/com/example/family/assignment/Transaction1Activity.java
@@ -1,13 +1,38 @@
 package com.example.family.assignment;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Transaction1Activity extends AppCompatActivity {
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction1);
+    }
+
+    public void openCamera(View v){
+        //open camera and scan qr code before proceed
+
+        intent = new Intent(this, Transaction2Activity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 }
